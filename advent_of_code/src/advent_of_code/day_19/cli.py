232d044e02bbeb380019564337f36a4a26d42fbb
Bryan Quach <bryancquach@gmail.com>
2025-11-10 23:54:06 +0000

    added directory structure for src code for days 5-25

---
+++ advent_of_code/src/advent_of_code/day_19/cli.py
@@ -0,0 +1,26 @@
+import typer
+from typing_extensions import Annotated
+
+app = typer.Typer(help="Day 19: [Puzzle Title]")
+
+
+@app.command()
+def run_part1(
+    data_file: Annotated[
+        str, typer.Argument(..., help="Path to input file.")
+    ],
+):
+    """Part 1 solution."""
+    # TODO: Implement solution
+    print("Part 1 not yet implemented")
+
+
+@app.command()
+def run_part2(
+    data_file: Annotated[
+        str, typer.Argument(..., help="Path to input file.")
+    ],
+):
+    """Part 2 solution."""
+    # TODO: Implement solution
+    print("Part 2 not yet implemented")
